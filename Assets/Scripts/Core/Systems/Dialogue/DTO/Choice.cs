402c6ee4b16972abbf91c5aaac4532d4124de135
adamskipadamski <adam.t.wright@outlook.com>
2024-11-03 15:43:49 +0000

    reorganise Scripts directory

--- Assets/Scripts/Core/Systems/Dialogue/DTO/Choice.cs
+++ Assets/Scripts/Core/Systems/Dialogue/DTO/Choice.cs
@@ -5,13 +5,15 @@ public class Choice
 {
     public string Text;
     public string Event;
+    public int RelationshipPoints;
     public string NextLineId;
     public string[] Requirements;
 
-    public Choice(string text, string @event, string nextLineId, string[] requirements)
+    public Choice(string text, string @event, int relationshipPoints, string nextLineId, string[] requirements)
     {
         Text = text;
         Event = @event;
+        RelationshipPoints = relationshipPoints;
         NextLineId = nextLineId;
         Requirements = requirements;
     }
