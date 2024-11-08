60968ae12c3d975a3a6e8bdf85cd9e3649e16f9d
adamskipadamski <adam.t.wright@outlook.com>
2024-11-08 14:27:34 +0000

    make TillView animate balance changes

--- Assets/Scripts/Till/TillView.cs
+++ Assets/Scripts/Till/TillView.cs
@@ -1,12 +1,47 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
 public class TillView : MonoBehaviour
 {
-    public TMP_Text Balance;
+    [SerializeField]
+    private TMP_Text VisibleBalance;
+
+    private int Balance;
+
+    private IEnumerator ChangeVisibleBalanceCoroutine;
+    private WaitForSecondsRealtime WaitBetweenChanges = new(0.0050f);
 
     public void Display(int amount)
     {
-        Balance.text = amount.ToString();    
+        if (ChangeVisibleBalanceCoroutine != null)
+        {
+            StopCoroutine(ChangeVisibleBalanceCoroutine);
+        }
+
+        ChangeVisibleBalanceCoroutine = ChangeVisibleBalance(amount);
+
+        StartCoroutine(ChangeVisibleBalanceCoroutine);
+    }
+
+    private IEnumerator ChangeVisibleBalance(int amount)
+    {
+        while (Balance != amount)
+        {
+            if (Balance < amount)
+            {
+                Balance++;
+            }
+            else if (Balance > amount)
+            {
+                Balance--;
+            }
+
+            VisibleBalance.text = Balance.ToString();
+
+            yield return WaitBetweenChanges;
+        }
+
+        ChangeVisibleBalanceCoroutine = null;
     }
 }
