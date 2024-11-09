56c13b7e0341be77dae4a038e49029c2d325a8a3
Megan <megan@theparkes.me.uk>
2024-11-09 11:21:32 +0000

    update bookshelf

---
+++ Assets/Scripts/Book/BookShelf.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+class BookShelf : MonoBehaviour
+{
+    public List<Book> books;
+
+    public GameObject ShelfOne;
+    public GameObject ShelfTwo;
+    public GameObject ShelfThree;
+
+    public void RenderShelves()
+    {
+        ClearAllShelves();
+
+
+    }
+
+    private void ClearAllShelves()
+    {
+        foreach (Transform child in ShelfOne.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in ShelfTwo.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in ShelfThree.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+
+}
