92dcc6af151902b553938074afb062553959e865
Megan <megan@theparkes.me.uk>
2024-11-10 16:40:02 +0000

    main menu

---
+++ Assets/Scripts/Core/EventOnHover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class EventOnHover : MonoBehaviour
+{
+    public UnityEvent OnHoverIn;
+    public UnityEvent OnHoverOut;
+
+    void OnMouseEnter()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            OnHoverIn?.Invoke();
+        }
+    }
+
+
+    void OnMouseExit()
+    {
+        OnHoverOut?.Invoke();
+    }
+}
