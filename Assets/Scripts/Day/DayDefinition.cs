88220ccd9bd527c65d666d1a2b0c6e049af70181
adamskipadamski <adam.t.wright@outlook.com>
2024-11-11 12:20:22 +0000

    remove VisitingCharacters from DayDefinition

--- Assets/Scripts/Day/DayDefinition.cs
+++ Assets/Scripts/Day/DayDefinition.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DayDefinition : ScriptableObject
 {
     public string Date;
-    public List<CharacterDefinition> VisitingCharacters;
     public List<GameEvent> DailyEvents;
     public bool IsInThePast;
 }
