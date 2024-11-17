25099af912fb3491f2fd04c1a03de56439de8c49
adamskipadamski <adam.t.wright@outlook.com>
2024-11-17 12:45:06 +0000

    only allow player to call supplier once per day

--- Assets/Scripts/Bookkeeper/Supplier.cs
+++ Assets/Scripts/Bookkeeper/Supplier.cs
@@ -4,10 +4,30 @@ public class Supplier : MonoBehaviour
 {
     public DayManager DayManager;
     public DialogueManager DialogueManager;
-    public Line NoAnswer;
+    public Line NoAnswerDialogue;
+
+    private bool HasSpokenToSupplier;
+
+    void Start()
+    {
+        EventManager.Instance.Subscribe(GameEventType.BEGIN_DAY, OnBeginDay);
+    }
+
+    private void OnBeginDay(GameEvent _)
+    {
+        HasSpokenToSupplier = false;
+    }
 
     public void Call()
     {
-        DialogueManager.Begin(DayManager.GetSupplierInteraction().RootLine, null);
+        if (!HasSpokenToSupplier)
+        {
+            HasSpokenToSupplier = true;
+            DialogueManager.Begin(DayManager.GetSupplierDialogue(), null);
+        }
+        else
+        {
+            DialogueManager.Begin(NoAnswerDialogue, null);
+        }
     }
 }
