51b4e914ce0d582575fb4805a5121a7a980b67f6
adamskipadamski <adam.t.wright@outlook.com>
2024-11-24 11:10:59 +0000

    fix picking book up animations

--- Assets/Scripts/Core/Clickable.cs
+++ Assets/Scripts/Core/Clickable.cs
@@ -6,16 +6,9 @@ public class Clickable : MonoBehaviour
 {
     public UnityEvent OnClick;
 
-    private Cursor Cursor;
-
-    void Start()
-    {
-        Cursor = FindFirstObjectByType<Cursor>();
-    }
-
     void OnMouseDown()
     {
-        if (!EventSystem.current.IsPointerOverGameObject())
+        if (enabled && !EventSystem.current.IsPointerOverGameObject())
         {
             OnClick?.Invoke();
         }
