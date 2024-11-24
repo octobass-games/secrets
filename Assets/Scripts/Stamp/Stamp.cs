f44c63c251b593872b846fff97caa1f273d67d23
Megan <megan@theparkes.me.uk>
2024-11-24 11:33:12 +0000

    start stamps UI

--- Assets/Scripts/Stamp/Stamp.cs
+++ Assets/Scripts/Stamp/Stamp.cs
@@ -3,4 +3,17 @@ using UnityEngine;
 public class Stamp : MonoBehaviour
 {
     public StampDefinition StampDefinition;
+    public GameObject StampUnlocked;
+    public GameObject StampLocked;
+
+    public void ShowUnlocked()
+    {
+        StampUnlocked.SetActive(true);
+        StampUnlocked.SetActive(false);
+    }
+    public void ShowLocked()
+    {
+        StampUnlocked.SetActive(false);
+        StampUnlocked.SetActive(true);
+    }
 }
