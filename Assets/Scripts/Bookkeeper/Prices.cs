a9c457aa8f8b1117961bd98b89e3df2a9f6bdf13
adamskipadamski <adam.t.wright@outlook.com>
2024-11-28 01:55:42 +0000

    add all filter options

--- Assets/Scripts/Bookkeeper/Prices.cs
+++ Assets/Scripts/Bookkeeper/Prices.cs
@@ -32,7 +32,23 @@ public class Prices : MonoBehaviour
     }
 
     public void ListAllBookPrices() => ListBookPrices(BookCategory.ALL, null);
+    public void ListArtBookPrices() => ListBookPrices(BookCategory.ART, null);
+    public void ListBiographyBookPrices() => ListBookPrices(BookCategory.BIOGRAPHY, null);
+    public void ListChildrensBookPrices() => ListBookPrices(BookCategory.CHILDRENS, null);
+    public void ListCraftsBookPrices() => ListBookPrices(BookCategory.CRAFTS, null);
+    public void ListEconomicsBookPrices() => ListBookPrices(BookCategory.ECONOMICS, null);
+    public void ListHealthBookPrices() => ListBookPrices(BookCategory.HEALTH, null);
+    public void ListHistoryBookPrices() => ListBookPrices(BookCategory.HISTORY, null);
+    public void ListHorrorBookPrices() => ListBookPrices(BookCategory.HORROR, null);
+    public void ListOccultBookPrices() => ListBookPrices(BookCategory.OCCULT, null);
+    public void ListPoetryBookPrices() => ListBookPrices(BookCategory.POETRY, null);
+    public void ListPotionsBookPrices() => ListBookPrices(BookCategory.POTIONS, null);
+    public void ListRecipesBookPrices() => ListBookPrices(BookCategory.RECIPES, null);
     public void ListRomanceBookPrices() => ListBookPrices(BookCategory.ROMANCE, null);
+    public void ListSpellsBookPrices() => ListBookPrices(BookCategory.SPELLS, null);
+    public void ListThrillerBookPrices() => ListBookPrices(BookCategory.THRILLER, null);
+    public void ListWeaponsBookPrices() => ListBookPrices(BookCategory.WEAPONS, null);
+    public void ListWellbeingBookPrices() => ListBookPrices(BookCategory.WELLBEING, null);
 
     private void ListBookPrices(BookCategory category, string title)
     {
