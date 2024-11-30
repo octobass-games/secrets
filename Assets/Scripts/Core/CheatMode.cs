ef6e284ca5e9d4a01f27e25189c5439c05eaf06e
Megan <megan@theparkes.me.uk>
2024-11-30 14:45:31 +0000

    update trader to velia

--- Assets/Scripts/Core/CheatMode.cs
+++ Assets/Scripts/Core/CheatMode.cs
@@ -16,20 +16,7 @@ public class CheatMode : MonoBehaviour
     public void SubmitCheat()
     {
         Debug.Log("Submit Cheat: " + CheatInput.text);
-        if (CheatInput.text.StartsWith("/skip-to-day ") || CheatInput.text.StartsWith("day "))
-        {
-            var dayString = CheatInput.text.Replace("/skip-to-day ", "").Replace("day ", "");
-            try
-            {
-                var day = int.Parse(dayString);
-                DayManager.SkipToDay(day);
-            }
-            catch
-            {
-
-            }
-        }
-        else if (CheatInput.text.Equals("motherlode"))
+        if (CheatInput.text.Equals("motherlode"))
         {
             EventManager.Instance.Publish(new GameEvent() { Type = GameEventType.BANK_DEPOSIT, Amount = 50000 });
         }
