e7fb46c8ce5622a9f5c986f2e781a492d9974b37
Megan <megan@theparkes.me.uk>
2024-11-30 13:41:58 +0000

    plug in fade on game overr

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class GameOver : MonoBehaviour
     public List<Camera> OtherCameras;
     public TMP_Text Date;
     public TMP_Text Headline;
+    public DayManager DayManager;
+    public Animator Fade;
 
     void OnEnable()
     {
@@ -21,11 +24,19 @@ public class GameOver : MonoBehaviour
 
     private void OnGameOver(GameEvent @event)
     {
-        Date.text = @event.Day.Date;
-        Headline.text = @event.Message;
+        Fade.SetTrigger("out");
+        Date.text = DayManager.GetToday().Date;
+        Headline.text = @event.Message;     
+        StartCoroutine(WaitForNSecondsThenSendFadeIn());
+    }
 
+    IEnumerator WaitForNSecondsThenSendFadeIn()
+    {
+        yield return new WaitForSeconds(1);
         OtherCameras.ForEach(camera => camera.gameObject.SetActive(false));
-        
+
         GameOverCamera.gameObject.SetActive(true);
+        Fade.SetTrigger("in");
+
     }
 }
