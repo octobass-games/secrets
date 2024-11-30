4b19c55b6c80d8b7dfffb6aa62bbeae247db8c5c
adamskipadamski <adam.t.wright@outlook.com>
2024-11-30 11:44:03 +0000

    make EventManager only live in scene it is created

--- Assets/Scripts/Core/Systems/Event/EventManager.cs
+++ Assets/Scripts/Core/Systems/Event/EventManager.cs
@@ -11,6 +11,8 @@ public class EventManager : MonoBehaviour
 
     void Awake()
     {
+        Debug.Log(Instance == null);
+
         if (Instance != null && Instance != this)
         {
             Destroy(this);
@@ -18,8 +20,12 @@ public class EventManager : MonoBehaviour
         else
         {
             Instance = this;
-            DontDestroyOnLoad(Instance);
         }
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("Goodbye!");
     }
 
     public void Subscribe(GameEventType eventType, EventHandler receiver)
