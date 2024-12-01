095be27cf620540d70663f3ffe8a16db8001f28c
adamskipadamski <adam.t.wright@outlook.com>
2024-12-01 01:01:59 +0000

    disable knife in hollow books

--- Assets/Scripts/BookInspector.cs
+++ Assets/Scripts/BookInspector.cs
@@ -64,6 +64,8 @@ public class BookInspector : MonoBehaviour
             {
                 DialogueManager.Begin(FirstInspectionOfHollowBookDialogue);
             }
+
+            Knife.SetActive(false);
         }
         else
         {
